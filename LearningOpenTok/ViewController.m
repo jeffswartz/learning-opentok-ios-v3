d0cb08acef0b165332f194b9e6a298967a905e8b
Lucas Huang <lucas@tokbox.com>
2017-04-13 14:46:29 -0700

    update image asset; modify Podfile to point to minimum 2.10.0

--- LearningOpenTok/ViewController.m
+++ LearningOpenTok/ViewController.m
@@ -134,9 +134,9 @@
     _publisher.publishAudio = !_publisher.publishAudio;
     UIImage *buttonImage;
     if (_publisher.publishAudio) {
-        buttonImage = [UIImage imageNamed: @"mic-24.png"];
+        buttonImage = [UIImage imageNamed: @"mic"];
     } else {
-        buttonImage = [UIImage imageNamed: @"mic_muted-24.png"];
+        buttonImage = [UIImage imageNamed: @"muted_mic"];
     }
     [_publisherAudioBtn setImage:buttonImage forState:UIControlStateNormal];
 }
@@ -146,9 +146,9 @@
     _subscriber.subscribeToAudio = !_subscriber.subscribeToAudio;
     UIImage *buttonImage;
     if (_subscriber.subscribeToAudio) {
-        buttonImage = [UIImage imageNamed: @"Subscriber-Speaker-35.png"];
+        buttonImage = [UIImage imageNamed: @"audio"];
     } else {
-        buttonImage = [UIImage imageNamed: @"Subscriber-Speaker-Mute-35.png"];
+        buttonImage = [UIImage imageNamed: @"noAudio"];
     }
     [_subscriberAudioBtn setImage:buttonImage forState:UIControlStateNormal];
 }
